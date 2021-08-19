5594898f8db882a790b3becafab6e49adde0b3c7
KaurManjot0432 <0432manjot@gmail.com>
2021-08-19 15:59:40 +0530

    Initiated auth views

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,11 @@
 module.exports.profile = function(req,res){
     return res.render('users/user_profile',{layout:__dirname+'/../views/layouts/user_layout'});
 }
+
+module.exports.signup = function(req,res){
+    return res.render('users/user_sign_up');
+}
+
+module.exports.signin = function(req,res){
+    return res.render('users/user_sign_in');
+}
