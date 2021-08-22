2ea23711038656a3dffd01bfb9a37ace262a685e
KaurManjot0432 <0432manjot@gmail.com>
2021-08-22 18:29:51 +0530

    tweet creation done

---
+++ src/controllers/tweetController.js
@@ -0,0 +1,16 @@
+const Tweet = require('../models/tweet');
+
+const create = function(req,res){
+    Tweet.create({
+        content : req.body.content,
+        user : req.body._id
+    },function(err,tweet){
+        if(err){
+            console.error("Error in creating the tweet");
+            return;
+        }
+        return res.redirect('back');
+    })
+}
+
+module.exports = {create};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const homeControllers = require('../controllers/homeConrollers');
 const router = express.Router();
 const userRouter = require('./user');
+const tweetRouter = require('./tweet');
 
 console.log('router UP!!');
 router.get('/',homeControllers.root);
 router.use('/users',userRouter);
+router.use('/tweets',tweetRouter);
 
 module.exports = router;
